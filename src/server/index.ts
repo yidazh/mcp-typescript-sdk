21f492f5317b9a6321c9488b308f72f3acf9cad6
Justin Spahr-Summers <justin@anthropic.com>
2025-04-22 11:35:49 +0100

    Revert "Merge pull request #334 from m-paternostro/mp/notifier"
    This reverts commit 3ef902328a940b06f666ac02422bbba1be4541ec, reversing
    changes made to 5b6d35eb12bebfe48f8fd160b56575ec8eac7a21.

--- src/server/index.ts
+++ src/server/index.ts
@@ -105,13 +105,6 @@ export class Server<
   }
 
   /**
-   * The server's name and version.
-   */
-  getVersion(): { readonly name: string; readonly version: string } {
-    return this._serverInfo;
-  }
-
-  /**
    * Registers new capabilities. This can only be called before connecting to a transport.
    *
    * The new capabilities will be merged with any existing capabilities previously given (e.g., at initialization).
