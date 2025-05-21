70f03ee648353898c288651e84304869981ac3dd
ihrpr <inna@anthropic.com>
2025-05-21 21:27:42 +0100

    remove non mac open browser commands

--- src/examples/client/simpleOAuthClient.ts
+++ src/examples/client/simpleOAuthClient.ts
@@ -112,13 +112,7 @@ class InteractiveOAuthClient {
     const platform = process.platform;
     let command: string;
 
-    if (platform === 'darwin') {
-      command = `open "${url}"`;
-    } else if (platform === 'win32') {
-      command = `start "" "${url}"`;
-    } else {
-      command = `xdg-open "${url}"`;
-    }
+    command = `open "${url}"`;
 
     exec(command, (error) => {
       if (error) {
